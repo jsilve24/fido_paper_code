e8c204407512ff9f4bb0df4b0525c5a69d8d7664
Kimberly Roche <ker48@x2-01-1.genome.duke.edu>
2018-10-29 10:24:18 -0400

    Added diagnostic plots for partial Hessian

--- diagnose_accuracy.R
+++ diagnose_accuracy.R
@@ -7,18 +7,19 @@ require(ggplot2)
 source("src/dataset_methods.R")
 source("src/plotting.R")
 
-load("fit_MC_N100_D30_Q100.RData")
-load("fit_ME_N100_D30_Q100.RData")
-load("fit_SC_N100_D30_Q100.RData")
+load("fitted_models/ME_N100_D30_Q5_R3.RData")
+load("fitted_models/MC_N100_D30_Q5_R3.RData")
+load("fitted_models/MCP_N100_D30_Q5_R3.RData")
+load("fitted_models/SC_N100_D30_Q5_R3.RData")
 
 N_ll <- 1
 N_ul <- 5 # max 100
 
 D_ll <- 1
-D_ul <- 5 # max 30
+D_ul <- 5 # max 29
 
 Q_ll <- 1
-Q_ul <- 10 # max 100
+Q_ul <- 5 # max 5
 
 print(paste("N range",N_ll,":",N_ul))
 print(paste("D range",D_ll,":",D_ul))
@@ -26,25 +27,26 @@ print(paste("Q range",Q_ll,":",Q_ul))
 
 do_plot <- TRUE
 
-# should all be the same and by quick crappy visual inspection they are
-#Lambda_true <- fit.mongrel.cholesky$mdataset$Lambda_true[D_ll:D_ul, Q_ll:Q_ul]
-#Lambda_true <- fit.mongrel.eigen$mdataset$Lambda_true[D_ll:D_ul, Q_ll:Q_ul]
+# should all be the same for all models
 Lambda_true <- fit.sc$mdataset$Lambda_true[D_ll:D_ul, Q_ll:Q_ul]
 
-MC <- fit.mongrel.cholesky
-ME <- fit.mongrel.eigen
+MC <- fit.mc
+MCP <- fit.mcp
+ME <- fit.me
 SC <- fit.sc
 
 # truncate the Lambdas and etas; we'll just look at a subset
 MC$Lambda <- MC$Lambda[D_ll:D_ul, Q_ll:Q_ul,]
+MCP$Lambda <- MCP$Lambda[D_ll:D_ul, Q_ll:Q_ul,]
 ME$Lambda <- ME$Lambda[D_ll:D_ul, Q_ll:Q_ul,]
 SC$Lambda <- SC$Lambda[D_ll:D_ul, Q_ll:Q_ul,]
 
 MC$Eta <- MC$Eta[D_ll:D_ul, N_ll:N_ul,]
+MCP$Eta <- MCP$Eta[D_ll:D_ul, N_ll:N_ul,]
 ME$Eta <- ME$Eta[D_ll:D_ul, N_ll:N_ul,]
 SC$Eta <- SC$Eta[D_ll:D_ul, N_ll:N_ul,]
 
-mfits <- list("fit.mc"=MC, "fit.me"=ME, "fit.sc"=SC)
+mfits <- list("fit.mc"=MC, "fit.mcp"=MCP, "fit.me"=ME, "fit.sc"=SC)
 
 # calculate MSE
 ref <- c(Lambda_true)
@@ -54,9 +56,10 @@ get_MSE <- function(est_Lambda, trunc_d1, trunc_d2, d3, ref) {
   return(mean(apply(est_Lambda, 2, sample_SE, y=ref)))
 }
 
-print(paste("MSE MC:",get_MSE(MC$Lambda, nrow(MC$Lambda), ncol(MC$Lambda), MC$iter, ref)))
-print(paste("MSE ME:",get_MSE(ME$Lambda, nrow(ME$Lambda), ncol(ME$Lambda), ME$iter, ref)))
-print(paste("MSE SC:",get_MSE(SC$Lambda, nrow(SC$Lambda), ncol(SC$Lambda), SC$iter, ref)))
+cat(paste("MSE MC:",get_MSE(MC$Lambda, nrow(MC$Lambda), ncol(MC$Lambda), MC$iter, ref),"\n"))
+cat(paste("MSE MCP:",get_MSE(MCP$Lambda, nrow(MCP$Lambda), ncol(MCP$Lambda), MCP$iter, ref),"\n"))
+cat(paste("MSE ME:",get_MSE(ME$Lambda, nrow(ME$Lambda), ncol(ME$Lambda), ME$iter, ref),"\n"))
+cat(paste("MSE SC:",get_MSE(SC$Lambda, nrow(SC$Lambda), ncol(SC$Lambda), SC$iter, ref),"\n"))
 
 # get count of Lambdas outside 95% CI
 get_outside_count <- function(est_Lambda, ref) {
@@ -68,13 +71,14 @@ get_outside_count <- function(est_Lambda, ref) {
   return(sum_outside)
 }
 
-print(paste("Outside 95 CI count MC:",get_outside_count(MC$Lambda, ref)))
-print(paste("Outside 95 CI count ME:",get_outside_count(ME$Lambda, ref)))
-print(paste("Outside 95 CI count SC:",get_outside_count(SC$Lambda, ref)))
+cat(paste("Outside 95 CI count MC:",get_outside_count(MC$Lambda, ref),"\n"))
+cat(paste("Outside 95 CI count MCP:",get_outside_count(MCP$Lambda, ref),"\n"))
+cat(paste("Outside 95 CI count ME:",get_outside_count(ME$Lambda, ref),"\n"))
+cat(paste("Outside 95 CI count SC:",get_outside_count(SC$Lambda, ref),"\n"))
 
 if(do_plot) {
-  plot_lambda(mfits, Lambda_true=Lambda_true, image_filename="Lambda_subset_plot_N100_D30_Q100.png")
-  plot_eta(mfits, Eta_true=NULL, image_filename="Eta_subset_plot_N100_D30_Q100.png")
+  plot_lambda(mfits, Lambda_true=Lambda_true, image_filename="Lambda_subset_plot_N100_D30_Q5_R3.png")
+  plot_eta(mfits, Eta_true=NULL, image_filename="Eta_subset_plot_N100_D30_Q5_R3.png")
 }
 
 
