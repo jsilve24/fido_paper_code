bae96de09c9f62968e93b1030a1c9e8a4c389ff3
Kim Roche <kimberly.e.roche@gmail.com>
2018-11-28 15:51:58 -0500

    Updated figure rendering

--- render_figures.R
+++ render_figures.R
@@ -8,7 +8,13 @@ library(ggstance)
 library(ggpubr)
 library(RColorBrewer)
 
-apply_common_settings <- function(base_p, horiz=FALSE, use_legend=FALSE, share_y=FALSE) {
+build_color_palette <- function() {
+  my_colors <- brewer.pal(4, "Set1")
+  names(my_colors) <- c("mongrel_cholesky", "stan_collapsed", "stan_uncollapsed", "conjugate_linear_model")
+  return(scale_colour_manual(name="model", values=my_colors))
+}
+
+apply_common_settings <- function(base_p, horiz=FALSE, use_legend=FALSE, share_y=FALSE, use_manual_color=TRUE) {
   # apply grid
   if(horiz) {
     p <- base_p + facet_grid(. ~ sweep_param, scales="free_x")
@@ -23,7 +29,6 @@ apply_common_settings <- function(base_p, horiz=FALSE, use_legend=FALSE, share_y
   # spacing
   p <- p + theme(panel.spacing = unit(0.5, "lines")) +
     theme(plot.margin=unit(c(0,0,0,0),"pt"))
-    # theme(aspect.ratio = 1)
 
   # apply theme and border
   p <- p + theme_minimal() +
@@ -35,16 +40,21 @@ apply_common_settings <- function(base_p, horiz=FALSE, use_legend=FALSE, share_y
 
   # remove legend and labels
   p <- p + xlab("") +
-    ylab("") +
-    theme(strip.text.x = element_blank()) +
-    theme(strip.text.y = element_blank())
+    ylab("")
+  if(!use_legend) {
+    p <- p + theme(strip.text.x = element_blank()) +
+        theme(strip.text.y = element_blank())
+  }
 
-  my_colors <- brewer.pal(4, "Set1")
-  names(my_colors) <- c("mongrel_cholesky", "stan_collapsed", "stan_uncollapsed", "conjugate_linear_model")
-  col_scale <- scale_colour_manual(name="model", values=my_colors)
-  # fix colors
-  # p <- p + scale_colour_brewer(palette = "Set1")
-  p <- p + col_scale
+  if(use_manual_color) {
+    col_scale <- build_color_palette()
+    p <- p + col_scale
+  } else {
+    p <- p + scale_colour_brewer(palette = "Set1")
+  }
+
+  # later, maybe?
+  # theme(aspect.ratio = 1)
 
   return(p)
 }
@@ -112,6 +122,7 @@ render_F1_C4 <- function(use_CLM=FALSE, use_legend=FALSE) {
 }
 
 render_F1 <- function(use_legend=FALSE) {
+  cat("Rendering Figure 1\n")
   dat <- read.csv("all_output.log")
   c1 <- render_F1_C1(dat, use_legend=FALSE)
 
@@ -121,19 +132,20 @@ render_F1 <- function(use_legend=FALSE) {
   c4 <- render_F1_C4(use_legend=use_legend)
   if(use_legend) {
     p <- ggarrange(c1, c2, c3, c4, ncol=4, nrow=1, widths = c(1, 1.5, 1.5, 1.5))
-    ggsave("figure_drafts/F1.png", plot=p, width=17, height=8, units="in")  
+    ggsave("figure_drafts/legends/F1.png", plot=p, width=17, height=8, units="in")  
   } else {
     p <- ggarrange(c1, c2, c3, c4, ncol=3, nrow=1, widths = c(1, 1, 1, 1))
-    ggsave("figure_drafts/F1.png", plot=p, width=10, height=8, units="in")  
+    ggsave("figure_drafts/no_legends/F1.png", plot=p, width=10, height=8, units="in")  
   }
 }
 
 render_S1 <- function(use_legend=FALSE) {
+  cat("Rendering Supplemental Figure 1\n")
   p <- render_F1_C4(use_CLM=TRUE, use_legend=use_legend)
   if(use_legend) {
-    ggsave("figure_drafts/S1.png", plot=p, width=5, height=8, units="in")
+    ggsave("figure_drafts/legends/S1.png", plot=p, width=5, height=8, units="in")
   } else {
-    ggsave("figure_drafts/S1.png", plot=p, width=8, height=8, units="in")
+    ggsave("figure_drafts/no_legends/S1.png", plot=p, width=8, height=8, units="in")
   }
 }
 
@@ -146,7 +158,7 @@ subset_Lambda <- function(mfits, lower, upper) {
   return(list("Lambda_true"=Lambda_true, "mfits"=mfits))
 }
 
-plot_posterior_intervals <- function(mfits, ll, ul, image_filename=NULL) {
+plot_posterior_intervals <- function(mfits, ll, ul, use_legend=FALSE, image_filename=NULL) {
   Lambda_true <- mfits$mongrel_cholesky$mdataset$Lambda_true[ll:ul,ll:ul]
   mfits$mongrel_cholesky$Lambda <- mfits$mongrel_cholesky$Lambda[ll:ul,ll:ul,]
   mfits$stan_collapsed$Lambda <- mfits$stan_collapsed$Lambda[ll:ul,ll:ul,]
@@ -173,24 +185,33 @@ plot_posterior_intervals <- function(mfits, ll, ul, image_filename=NULL) {
     theme(axis.text.y=element_blank()) +
     theme(strip.text.x=element_blank()) + 
     theme(strip.text.y=element_blank()) + 
-    theme(panel.spacing=unit(0.5, "lines")) +
-    theme(legend.position="none") +
+    theme(panel.spacing=unit(0.5, "lines")) + 
     theme(panel.border = element_rect(color="black", fill=NA, size=0.5))
+  if(!use_legend) {
+    p <- p + theme(legend.position="none")
+  }
+  col_scale <- build_color_palette()
+  p <- p + col_scale
   if(!is.null(image_filename)) {
-    ggsave(image_filename, plot=p, width=8, height=8, dpi=300)
+    if(use_legend) {
+      ggsave(paste("figure_drafts/legends/",image_filename,sep=""), plot=p, width=10, height=8, dpi=300)
+    } else {
+      ggsave(paste("figure_drafts/no_legends/",image_filename,sep=""), plot=p, width=8, height=8, dpi=300)
+    }
   }
 }
 
-render_SF1 <- function() {
- load("fitted_models/MC_N100_D30_Q250_R1.RData")
- load("fitted_models/SC_N100_D30_Q250_R1.RData")
- load("fitted_models/SU_N100_D30_Q250_R1.RData")
- load("fitted_models/CLM_N100_D30_Q250_R1.RData")
- mfits <- list("mongrel_cholesky"=fit.mc,
+render_S2 <- function(use_legend=FALSE) {
+  cat("Rendering Supplemental Figure 2\n")
+  load("fitted_models/MC_N100_D30_Q250_R1.RData")
+  load("fitted_models/SC_N100_D30_Q250_R1.RData")
+  load("fitted_models/SU_N100_D30_Q250_R1.RData")
+  load("fitted_models/CLM_N100_D30_Q250_R1.RData")
+  mfits <- list("mongrel_cholesky"=fit.mc,
                "stan_collapsed"=fit.sc,
                "stan_uncollapsed"=fit.su,
                "conjugate_linear_model"=fit.clm)
- plot_posterior_intervals(mfits, 1, 5, image_filename="figure_drafts/SF1_fail_case.png")
+  plot_posterior_intervals(mfits, 1, 5, use_legend=use_legend, image_filename="S2_fail_case.png")
 
   load("fitted_models/MC_N30_D30_Q5_R1.RData")
   load("fitted_models/SC_N30_D30_Q5_R1.RData")
@@ -200,28 +221,56 @@ render_SF1 <- function() {
                 "stan_collapsed"=fit.sc,
                 "stan_uncollapsed"=fit.su,
                 "conjugate_linear_model"=fit.clm)
-  plot_posterior_intervals(mfits, 1, 5, image_filename="figure_drafts/SF1_good_case.png")
+  plot_posterior_intervals(mfits, 1, 5, use_legend=use_legend, image_filename="S2_good_case.png")
+}
+
+extract_opt_cols <- function(df, old_colname, new_colname) {
+  d <- select(df, c("sweep_param",
+                      "sweep_value",
+                      old_colname)) %>%
+    mutate(opt_part=new_colname)
+  colnames(d)[which(colnames(d)==old_colname)] <- "value"
+  return(d)
 }
 
 # proportion of runtime taken up by optimization 
-render_S2 <- function() {
+render_S3 <- function(use_legend=FALSE) {
+  cat("Rendering Supplemental Figure 3\n")
   dat <- read.csv("all_output.log")
-  dat_filtered <- filter(dat, model %in% c("mongrel_eigen", "mongrel_cholesky"))
-  p <- dat_filtered %>% 
-    mutate(pOpt=optimization_runtime/sample_runtime) %>% 
-    ggplot(aes(x=sweep_value, y=pOpt, color=model)) +
-    geom_point() +
+  dat <- filter(dat, model %in% c("mongrel_cholesky"))
+  dat <- select(dat, c("sweep_param",
+                       "sweep_value",
+                       "Timer_HessianCalculation",
+                       "Timer_LaplaceApproximation",
+                       "Timer_Optimization",
+                       "Timer_Overall",
+                       "Timer_Uncollapse_Overall.Overall"))
+  d1 <- extract_opt_cols(dat, "Timer_HessianCalculation", "HessianCalculation")
+  d2 <- extract_opt_cols(dat, "Timer_LaplaceApproximation", "LaplaceApproximation")
+  d3 <- extract_opt_cols(dat, "Timer_Optimization", "Optimization")
+  d4 <- extract_opt_cols(dat, "Timer_Overall", "Overall")
+  d5 <- extract_opt_cols(dat, "Timer_Uncollapse_Overall.Overall", "Uncollapse_Overall.Overall")
+  dat <- rbind(d1, d2, d3, d4, d5)
+  p <- dat %>%
+    ggplot(aes(x=sweep_value, y=value, color=opt_part)) +
+    geom_point() + 
+    geom_smooth(method="loess", se=FALSE) + 
     scale_x_log10() +
-    ylim(0, 1) +
-    facet_grid(. ~ sweep_param, scales="free_x")
-  p <- apply_common_settings(p, horiz=TRUE, use_legend=TRUE)
-  ggsave("figure_drafts/SF2_optimization_percent.png", plot=p, width=8, height=2.5, dpi=300)
+    scale_y_log10(
+      breaks = scales::trans_breaks("log10", function(x) 10^x),
+      labels = scales::trans_format("log10", scales::math_format(10^.x))
+    )
+#    facet_grid(. ~ sweep_param, scales="free_x")
+  p <- apply_common_settings(p, horiz=TRUE, use_legend=use_legend, use_manual_color=FALSE)
+  if(use_legend) {
+    ggsave("figure_drafts/legends/S3.png", plot=p, width=8, height=2.5, dpi=300)
+  } else {
+    ggsave("figure_drafts/no_legends/S3.png", plot=p, width=8, height=3, dpi=300)
+  }
 }
 
 render_F1(use_legend=TRUE)
 render_S1(use_legend=TRUE)
-# render_F2()
-#render_F2(use_stan_collapsed=FALSE)
-#render_SF1()
-#render_S2()
+render_S2(use_legend=TRUE)
+render_S3(use_legend=TRUE)
 
